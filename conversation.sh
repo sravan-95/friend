02993685b401f544e0b2653e02fa2903ca4ca4b2
sravann <sravanbasis55@gmail.com>
2026-06-02 21:14:23 +0530

    new script

---
+++ conversation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=TRUMP
+PERSON2=Iran
+
+echo "$PERSON1 : Hey $PERSON2, we are going to blast you"
+echo "$PERSON2 : Yes please, you are welcome"
+echo "$PERSON1 : We are going to blast you with our new missiles"
+echo "$PERSON2 : MR $PERSON1, we are ready to face you"
+echo "$PERSON1 : hey $PERSON2, i am serious"
+echo "$PERSON2 : Hey $PERSON1, we are also serious"
