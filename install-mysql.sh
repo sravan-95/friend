b9063ce2d2926557cd3357de3233461c34ff688f
sravann <sravanbasis55@gmail.com>
2026-06-06 17:31:51 +0530

    install-mysql

---
+++ install-mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ];then
+echo "please run the script with root access"
+exit 1
+fi  
+
+dnf list installed mysql 
+if [ $? -eq 0 ]; then
+echo "mysql is already installed"
+else
+echo "install mysql"
+dnf insall mysql -y
+fi
