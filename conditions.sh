031534cb15b3463bc22bf1996243f02de8348b9a
sravann <sravanbasis55@gmail.com>
2026-06-06 15:09:42 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -1,7 +1,7 @@
 if [ $NUMBER -gt 30 ]; then
-    echo "the $NUMBER is greater than 30"
+    echo "given number $NUMBER is greater than 30"
 elif [ $NUMBER -eq 30 ]; then
-    echo "the $NUMBER is equal to 30"
+    echo "given number $NUMBER is equal to 30"
 else
     echo "given number $NUMBER is less than 30"
 fi
