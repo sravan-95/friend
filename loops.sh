837296bff2ea25f86d8c2eb4d8abc48832be6642
sravann <sravanbasis55@gmail.com>
2026-06-08 21:04:28 +0530

    install-mysql

--- loops.sh
+++ loops.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 USERID=$(id -u)
-LOGS_DIR=/var/log/shell-scriptLOGS_FILE="$LOGS_DIR/$0.log"
+LOGS_DIR=/var/log/shell-script
+LOGS_FILE="$LOGS_DIR/$0.log"
+timestamp=$(date "%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ];then
 echo "please run the script with root access"
@@ -9,10 +11,10 @@ fi
 
 value(){
     if [ $2 -ne 0 ]; then
-      echo "$1 installation failed" | tee -a $LOGS_FILE
+      echo "$timestamp $1 installation failed" | tee -a $LOGS_FILE
       exit 1
       else 
-      echo "installing $1 is successful" | tee -a $LOGS_FILE
+      echo "$timestamp installing $1 is successful" | tee -a $LOGS_FILE
     fi
 }
 
@@ -20,4 +22,11 @@ value(){
 for package in $@
 do
    echo "installing $package"
+   dnf list installed $package &>> $LOGS_FILE
+      if [ $? -ne 0 ]; then
+         dnf install $package -y &>> $LOGS_FILE
+         value "installing $package" $?
+      else  
+           echo "$timestamp $package is already installed" | tee -a $LOGS_FILE
+      fi  
 done
